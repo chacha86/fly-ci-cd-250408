8cbd958b81e3792318dcfe8b24843d91949e8c54
chacha86 <taejincha0619@gmail.com>
2025-04-08 12:25:14 +0900

    logger

--- src/main/kotlin/com/example/fly_ci_cd/domain/home/home/controller/HomeController.kt
+++ src/main/kotlin/com/example/fly_ci_cd/domain/home/home/controller/HomeController.kt
@@ -1,5 +1,6 @@
 package com.example.fly_ci_cd.domain.home.home.controller
 
+import com.example.fly_ci_cd.global.extensions.logger
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -8,11 +9,17 @@ import java.net.InetAddress
 @Controller
 class HomeController {
 
+    private val logger = logger()
+    
     @GetMapping
     @ResponseBody
     fun main(): String {
         val localhost = InetAddress.getLocalHost()
         val hostname = localhost.hostName
+
+        println(hostname) // 모든 환경
+        logger.info("hostname: $hostname") // info 레벨 설정 환경
+        logger.debug("hostname: $hostname") // debug 레벨 설정 환경
 
         return "홈, hostname: $hostname"
     }
